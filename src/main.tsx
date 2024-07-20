1e194c3817091f56325d822ee11bb132660ac93a
Brian Le <brianle239@gmail.com>
2024-07-20 01:03:12 -0700

    testing pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// import { HashRouter } from 'react-router-dom'
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <div>Hello world!</div>,
+//   },
+// ]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode>    
+    <App /> 
   </React.StrictMode>,
 )
---
+++ src/pages/Game1.tsx
@@ -0,0 +1,8 @@
+
+export default function Game1() {
+    return (
+        <div>
+        Game1...
+        </div>
+    )
+}
