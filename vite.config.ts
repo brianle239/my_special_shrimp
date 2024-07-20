1df3442dc47ab0a5c1341751eea4b0732cb8ca09
Brian Le <brianle239@gmail.com>
2024-07-19 23:16:29 -0700

    added dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://brianle239.github.io/my_special_shrimp/',
   plugins: [react()],
 })
