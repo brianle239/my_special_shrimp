396b0f652c0f4cfda2bb5c3a1bbd7e8dd9c6b15b
Brian Le <brianle239@gmail.com>
2024-07-20 01:28:06 -0700

    chaged base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: 'https://brianle239.github.io/my_special_shrimp/',
+  base: '/my_special_shrimp/',
   plugins: [react()],
 })
